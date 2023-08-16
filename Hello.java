bf085094766af13d686dac6cdd423266d83483bc
Steven Bruce Asis <114350433+tebenbrus@users.noreply.github.com>
2023-08-16 11:47:54 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+Public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, Software Engineering Course!")
+      System.out.println("My name is ASIS, Steven Bruce!");
+  }
+}
