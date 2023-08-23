d0eaf36d987f6c57d8226a4074539405489d0cda
tebenbrus <bruceasis0@gmail.com>
2023-08-23 16:08:07 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ Public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!")
       System.out.println("My name is ASIS, Steven Bruce!");
+	System.out.println ("Complicated world of SoftDev that is why it pays much!");
+	
   }
 }
