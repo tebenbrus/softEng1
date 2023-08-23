43a30f0add341db9233c66d900d1f7c6449978a5
Steven Bruce Asis <114350433+tebenbrus@users.noreply.github.com>
2023-08-23 16:09:23 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 Public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!")
-      System.out.println("My name is ASIS, Steven Bruce!");
-	System.out.println ("Complicated world of SoftDev that is why it pays much!");
-	
+      System.out.println("My name is ASIS, Steven Bruce!");	
   }
 }
